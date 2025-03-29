4031e55072e952b0ef82d195070d27704c6b0a3f
StodolaR <sto.dy@seznam.cz>
2025-03-29 09:28:19 +0100

    Add project files.

---
+++ Piskvorky/PlayBoard.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Piskvorky
+{
+    /// <summary>
+    /// Interaction logic for PlayBoard.xaml
+    /// </summary>
+    public partial class PlayBoard : UserControl
+    {
+        private PlayBoardState pbState;
+        private int noOfMoves;
+        private const int fieldSize = 20;
+        public PlayBoard()
+        {
+            InitializeComponent();
+            pbState = new PlayBoardState();
+            noOfMoves = 0;
+            InsertFields(canBoard);
+        }
+        private void InsertFields(Canvas board)
+        {
+            for (int j = 0; j < pbState.PlayBoardFields.GetLength(1); j++)
+            {
+                for (int i = 0; i < pbState.PlayBoardFields.GetLength(0); i++)
+                {
+                    Button field = new Button
+                    {
+                        Height = fieldSize,
+                        Width = fieldSize,
+                    };
+                    field.Click += Field_Click;
+                    field.Tag = i + "," + j;
+                    board.Children.Add(field);
+                    Canvas.SetLeft(field, i * fieldSize);
+                    Canvas.SetTop(field, j * fieldSize);
+                }
+            }
+        }
+        private void Field_Click(object sender, RoutedEventArgs e)
+        {
+            Button field = (Button)sender;
+            string[] coordinates = ((string)field.Tag).Split(',');
+            int X = int.Parse(coordinates[0]);
+            int Y = int.Parse(coordinates[1]);
+            if (!pbState.IsFree(X,Y)) return;
+            noOfMoves++;
+            int player;
+            if(noOfMoves % 2 == 0)
+            {
+                field.Content = "O";
+                player = 2;
+            }
+            else
+            {
+                field.Content = "X";
+                player = 1;
+            }
+            bool win = pbState.EvaluateMove(X,Y, player);
+            if (win)
+            {
+                MessageBox.Show($"Vyhrál Hráč{player}");
+                foreach (Button button in canBoard.Children)
+                {
+                    button.Content = "";
+                }
+                pbState.PlayboardReset();
+                noOfMoves = 0;
+            }
+        }
+
+        private void btnStav_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder scoreBoardBuild = new StringBuilder(); 
+            for (int j = 0; j < pbState.PlayBoardFields.GetLength(1); j++)
+            {
+                for (int i = 0; i < pbState.PlayBoardFields.GetLength(0); i++)
+                {
+                    scoreBoardBuild.AppendFormat("{0,3}", pbState.PlayBoardFields[i,j]);
+                }
+                scoreBoardBuild.Append(Environment.NewLine);
+            }
+            string scoreBoard = scoreBoardBuild.ToString();
+            MessageBox.Show(scoreBoard);
+        }
+    }
+}
---
+++ Piskvorky/PlayBoardState.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piskvorky
+{
+    internal class PlayBoardState
+    {
+        public int[,] PlayBoardFields { get; set; }
+        public PlayBoardState()
+        {
+            PlayBoardFields = new int[20, 20];
+            PlayboardReset();
+        }
+        public bool IsFree(int X, int Y)
+        {
+            return PlayBoardFields[X, Y] > 2;
+        }
+        public bool EvaluateMove(int X, int Y, int player)
+        {
+            PlayBoardFields[X, Y] = player;
+            for (int direction = 0; direction < 4; direction++)
+            {
+                int charsInRows = 1;
+                int directionX = 0;
+                int directionY = 0;
+                switch (direction)
+                {
+                    case 0: directionX = -1; break;
+                    case 1: directionX = -1; directionY = -1; break;
+                    case 2: directionY = -1; break;
+                    case 3: directionX = 1; directionY = -1; break;
+                }
+                for (int shift = 1; shift <= 4; shift++)
+                {
+                    int shiftX = shift * directionX;
+                    int shiftY = shift * directionY;
+                    if (X - shiftX >= 0 && X - shiftX < PlayBoardFields.GetLength(0) 
+                        && Y - shiftY >= 0 && Y - shiftY < PlayBoardFields.GetLength(1))
+                    {
+                        if (PlayBoardFields[X - shiftX, Y - shiftY] == player)
+                        {
+                            charsInRows++;
+                        }
+                        else break;
+                    }
+                    else break;
+                }
+                for (int shift = 1; shift < 5; shift++)
+                {
+                    int shiftX = shift * directionX;
+                    int shiftY = shift * directionY;
+                    if (X + shiftX >= 0 && X + shiftX < PlayBoardFields.GetLength(0)
+                        && Y + shiftY >= 0 && Y + shiftY < PlayBoardFields.GetLength(1))
+                    {
+                        if (PlayBoardFields[X + shiftX, Y+ shiftY] == player)
+                        {
+                            charsInRows++;
+                        }
+                        else break;
+                    }
+                    else break;
+                }
+                if (charsInRows >= 5)
+                    return true;               
+            }
+            return false;
+        }
+        public void PlayboardReset()
+        {
+            for (int j = 0; j < PlayBoardFields.GetLength(1); j++)
+            {
+                for (int i = 0; i < PlayBoardFields.GetLength(0); i++)
+                {
+                    // 3 = clear field without score
+                    PlayBoardFields[i, j] = 3;
+                }
+            }
+        }
+    }
+}
